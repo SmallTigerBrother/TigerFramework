a5ee6c981dfe11ad197e2c12ec332bb61599a93f
tiger <664674103@qq.com>
2014-09-26 15:08:52 +0800

    删除部分无效代码

--- Tiger/src/com/mn/tiger/app/TGApplication.java
+++ Tiger/src/com/mn/tiger/app/TGApplication.java
@@ -1,16 +1,10 @@
 package com.mn.tiger.app;
 
-import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 
 import android.app.Activity;
 import android.app.Application;
-import android.content.Context;
-
-import com.mn.tiger.system.AppConfiguration;
-import com.mn.tiger.task.invoke.TGTaskInvoker;
-import com.mn.tiger.utility.LogTools;
 
 /**
  * 该类作用及功能说明 应用App类
@@ -36,60 +30,12 @@ public class TGApplication extends Application implements IApplication
 	{
 		super.onCreate();
 		instance = this;
-		// 初始化配置项
-		initConfiguration(getApplicationContext());
-		
-		// 启动crash日志捕获及上传监听
-		initCrashManager();
 	}
 
 	/** 得到 BaseApplication实例 */
 	public static IApplication getInstance()
 	{
 		return instance;
-	}
-
-	/**
-	 * 该方法的作用:初始化应用配置信息
-	 * 
-	 * @date 2014年1月3日
-	 * @param context
-	 */
-	private void initConfiguration(Context context)
-	{
-		try
-		{
-			// 此方法的调用需要在所有获取配置设置值之前
-			AppConfiguration.getInstance().loadSystemConfig(context);
-			initLogConfiguration(context);
-		}
-		catch (Exception e)
-		{
-			LogTools.e(LOG_TAG, "", e);
-		}
-	}
-	
-	/**
-	 * 该方法的作用:初始化日志的配置信息(默认存放sdcard路径:MEAP/包名，根据配置文件判定是否打开打印
-	 * 生产环境，PROGRESS和ERROR的级别日志写入文件，但不写控制台；其他环境日志文件打印到控制台，也写入文件。)
-	 * 
-	 * @date 2014年3月14日
-	 * @param context
-	 */
-	protected void initLogConfiguration(Context context)
-	{
-		LogTools.setAppLogFileDir("MEAP" + File.separator + context.getPackageName());
-		LogTools.switchLog(context);
-	}
-
-	/**
-	 * 该方法的作用:
-	 * 
-	 * @date 2014年5月23日
-	 */
-	protected void initCrashManager()
-	{
-		//TODO
 	}
 
 	/**
@@ -123,7 +69,6 @@ public class TGApplication extends Application implements IApplication
 	public void exit()
 	{
 		finishAllActivity();
-		TGTaskInvoker.unConnectRemoteService(this);
 		System.exit(0);
 	}
 
@@ -146,15 +91,5 @@ public class TGApplication extends Application implements IApplication
 		}
 
 		activities.clear();
-	}
-
-	/**
-	 * 该方法的作用: 连接到远程服务
-	 * 
-	 * @date 2014年5月23日
-	 */
-	protected void connectedRemoteService(String action)
-	{
-		AppConfiguration.setRunAtService(this, action);
 	}
 }
