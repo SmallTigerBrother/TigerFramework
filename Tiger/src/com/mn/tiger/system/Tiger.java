a18b018da4109e5004cebbd2ab34a532bd3da854
tiger <664674103@qq.com>
2014-09-28 17:11:42 +0800

    修改版本号

--- Tiger/src/com/mn/tiger/system/Tiger.java
+++ Tiger/src/com/mn/tiger/system/Tiger.java
@@ -11,25 +11,16 @@ public class Tiger
 	/**
 	 * 当前使用版本
 	 */
-	public static final VERSION_CODES SDK_CODE = VERSION_CODES.CARROT;
+	public static final VERSION_CODES SDK_CODE = VERSION_CODES.SOUTH_CHINA_TIGER;
 	
 	/**
-	 * 
 	 * 该类作用及功能说明:版本代号
 	 * 
 	 * @date 2014年4月26日
 	 */
 	public static enum VERSION_CODES
 	{
-		PENGUIN("1.5.0", 1), 
-		CANDY("2.0.0", 2), 
-		PILLOW("2.0.1", 3), 
-		CALENDAR("2.1.0", 4), 
-		CAKE("2.1.1", 5),
-		ZONGZI("2.2.0", 6),
-		APRICOT("2.2.1", 7),
-		SUGARCANE("2.3.0", 8),
-		CARROT("3.0.0", 9);
+		SOUTH_CHINA_TIGER("1.0.0", 1);
 		
 		// release版本号
 		public String RELEASE;
