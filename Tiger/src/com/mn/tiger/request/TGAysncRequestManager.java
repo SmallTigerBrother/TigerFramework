24d1098718fea0cfd3dd1ded326dc58507734372
SmallTigerBrother <664674103@qq.com>
2014-10-16 22:43:46 +0800

    新增异步请求管理类

---
+++ Tiger/src/com/mn/tiger/request/TGAysncRequestManager.java
@@ -0,0 +1,24 @@
+package com.mn.tiger.request;
+
+import android.content.Context;
+
+import com.mn.tiger.request.async.TGHttpAsyncRequester;
+
+public class TGAysncRequestManager
+{
+	public static void restartAllPausedRequest()
+	{
+		
+	}
+	
+	public static void cancelAllRequest(Context context)
+	{
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static void requestOrderly(TGHttpAsyncRequester[] httpAsyncRequesters)
+	{
+		
+	}
+	
+}
