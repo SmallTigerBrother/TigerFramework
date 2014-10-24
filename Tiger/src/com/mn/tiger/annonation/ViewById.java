4b166648e08e9f6acccc43cf40c69c6a81bb2ae8
tiger <664674103@qq.com>
2014-10-24 18:06:04 +0800

    提交ViewById注解，提交View注入器

---
+++ Tiger/src/com/mn/tiger/annonation/ViewById.java
@@ -0,0 +1,13 @@
+package com.mn.tiger.annonation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME) 
+public @interface ViewById 
+{
+	public int id();
+}
---
+++ Tiger/src/com/mn/tiger/utility/ViewInjector.java
@@ -0,0 +1,43 @@
+package com.mn.tiger.utility;
+
+import java.lang.reflect.Field;
+
+import com.mn.tiger.annonation.ViewById;
+
+import android.app.Activity;
+import android.view.View;
+
+
+public class ViewInjector
+{
+	public static void initInjectedView(Object injectedSource, View sourceView)
+	{
+		Field[] fields = injectedSource.getClass().getDeclaredFields();
+		if (fields != null && fields.length > 0)
+		{
+			for (Field field : fields)
+			{
+				ViewById viewById = field.getAnnotation(ViewById.class);
+				if (viewById != null)
+				{
+					int viewId = viewById.id();
+					try
+					{
+						field.setAccessible(true);
+						field.set(injectedSource, sourceView.findViewById(viewId));
+					}
+					catch (Exception e)
+					{
+						LogTools.e("Can not find" + field.getName() + " by id " + viewId + 
+								", please check if you was used wrong View");
+					}
+				}
+			}
+		}
+	}
+	
+	public static void initInjectedView(Object injectedSource, Activity activity)
+	{
+		initInjectedView(injectedSource, activity.getWindow().getDecorView());
+	}
+}
