4d643eeafa6209012e7c49f01f4c524e76a1d5f2
tiger <664674103@qq.com>
2014-11-20 16:51:40 +0800

    还原部分代码

--- Tiger/src/com/mn/tiger/request/async/task/TGGetTask.java
+++ Tiger/src/com/mn/tiger/request/async/task/TGGetTask.java
@@ -1,10 +1,5 @@
 package com.mn.tiger.request.async.task;
 
-import java.net.HttpURLConnection;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import com.mn.tiger.request.TGHttpRequester;
 import com.mn.tiger.request.receiver.TGHttpResult;
 
@@ -18,25 +13,7 @@ public class TGGetTask extends TGHttpTask
 	@Override
 	protected TGHttpResult executeHttpRequest()
 	{
-		TGHttpResult httpResult = new TGHttpResult();
-		httpResult.setResponseCode(HttpURLConnection.HTTP_OK);
-		JSONObject jsonObject = new JSONObject();
-		try
-		{
-			jsonObject.put("Test", "AAA0");
-			jsonObject.put("count", 120);
-			httpResult.setResult(jsonObject.toString());
-			
-			return httpResult;
-		}
-		catch (JSONException e)
-		{
-			e.printStackTrace();
-		}
-		
-		return httpResult;
-		
-//		return TGHttpRequester.requestGet(getContext(), getRequestUrl(), 
-//				getRequestParams(), getRequestProperties());
+		return TGHttpRequester.requestGet(getContext(), getRequestUrl(), 
+				getRequestParams(), getRequestProperties());
 	}
 }
