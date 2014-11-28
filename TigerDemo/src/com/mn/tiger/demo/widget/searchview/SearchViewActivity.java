035a2d8bf73e1008791ee036ccba67df34b792fb
tiger <664674103@qq.com>
2014-11-28 15:39:44 +0800

    实现搜索条相关示例

--- TigerDemo/src/com/mn/tiger/demo/widget/searchview/SearchViewActivity.java
+++ TigerDemo/src/com/mn/tiger/demo/widget/searchview/SearchViewActivity.java
@@ -1,9 +1,100 @@
 package com.mn.tiger.demo.widget.searchview;
 
 
+import android.os.Bundle;
+import android.text.TextUtils;
+
+import com.mn.tiger.annonation.ViewById;
 import com.mn.tiger.app.TGActionBarActivity;
+import com.mn.tiger.demo.R;
+import com.mn.tiger.utility.ToastUtils;
+import com.mn.tiger.utility.ViewInjector;
+import com.mn.tiger.widget.TGSearchView;
+import com.mn.tiger.widget.TGSearchView.OnQueryTextListener;
+import com.mn.tiger.widget.pulltorefresh.library.PullToRefreshBase.Mode;
+import com.mn.tiger.widget.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.mn.tiger.widget.pulltorefresh.library.PullToRefreshListView;
 
-public class SearchViewActivity extends TGActionBarActivity
+/**
+ * TGSearchView与PullToRefreshListView配合使用，Adapter注释
+ */
+public class SearchViewActivity extends TGActionBarActivity implements 
+    OnQueryTextListener, OnRefreshListener
 {
+	@ViewById(id = R.id.searchview)
+	private TGSearchView searchView;
+	
+	@ViewById(id = R.id.search_listview)
+	private PullToRefreshListView listView;
+	
+//	private TGListAdapter<T> listAdapter;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.searchview_activity);
+		
+		ViewInjector.initInjectedView(this, this);
+		setupViews();
+	}
+	
+	/**
+	 * 初始化视图
+	 */
+	protected void setupViews()
+	{
+		//初始化seachview
+		searchView.setOnQueryTextListener(this);
+		searchView.setQueryHint("请输入关键字");
+		
+		//初始化PullToRefreshListView
+		listView.setOnRefreshListener(this);
+		listView.setMode(Mode.PULL_FROM_END);
+//		listAdapter = new TGListAdapter<T>(this, null, onvertViewLayoutId, viewHolderClass);
+//		listView.setAdapter(listAdapter);
+	}
 
+	@Override
+	public void onQueryTextChange(CharSequence queryText)
+	{
+		
+	}
+
+	@Override
+	public void onQueryTextSubmit(CharSequence queryText)
+	{
+		if(!TextUtils.isEmpty(queryText))
+		{
+			requestSearch(queryText.toString(), 0);
+			return;
+		}
+		
+		ToastUtils.showToast(this, "您输入的内容不能为空");
+	}
+
+	@Override
+	public void onTextCleaned()
+	{
+		
+	}
+	
+	private void requestSearch(String queryText, int referId)
+	{
+		
+	}
+
+	@Override
+	public void onPullDownToRefresh()
+	{
+		//TODO 搜索请求,referId，为列表最后一行数据的标识，根据服务自行确定
+//		listAdapter.getLastItem();
+		requestSearch(searchView.getQueryText(), 0);
+	}
+
+	@Override
+	public void onPullUpToRefresh()
+	{
+		//不实现
+	}
 }
--- TigerDemo/src/com/mn/tiger/demo/widget/searchview/actionbar/SearchViewInActionBarActivity.java
+++ TigerDemo/src/com/mn/tiger/demo/widget/searchview/actionbar/SearchViewInActionBarActivity.java
@@ -1,6 +1,105 @@
 package com.mn.tiger.demo.widget.searchview.actionbar;
 
-public class SearchViewInActionBarActivity
-{
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.text.TextUtils;
 
+import com.mn.tiger.annonation.ViewById;
+import com.mn.tiger.app.TGActionBarActivity;
+import com.mn.tiger.demo.R;
+import com.mn.tiger.utility.DisplayUtils;
+import com.mn.tiger.utility.ToastUtils;
+import com.mn.tiger.utility.ViewInjector;
+import com.mn.tiger.widget.TGNavigationBar;
+import com.mn.tiger.widget.TGSearchView;
+import com.mn.tiger.widget.TGSearchView.OnQueryTextListener;
+import com.mn.tiger.widget.xlistview.XListView;
+import com.mn.tiger.widget.xlistview.XListView.IXListViewListener;
+
+public class SearchViewInActionBarActivity extends TGActionBarActivity implements 
+    OnQueryTextListener, IXListViewListener
+{
+	private TGSearchView searchView;
+	
+	@ViewById(id = R.id.search_xlistview)
+	private XListView listView;
+	
+//	private TGListAdapter<T> listAdapter;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setNavigationBarVisible(true);
+		setContentView(R.layout.searchview_in_actionbar_activity);
+		
+		ViewInjector.initInjectedView(this, this);
+		
+		setupViews();
+	}
+	
+	private void setupViews()
+	{
+		// 添加searchview，将searchview直接加入到navigationbar中
+		TGNavigationBar navigationBar = getNavigationBar();
+		navigationBar.removeAllViews();
+		searchView = new TGSearchView(this);
+		TGNavigationBar.LayoutParams layoutParams = new TGNavigationBar.LayoutParams(
+				ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+		layoutParams.bottomMargin = DisplayUtils.dip2px(this, 4);
+		layoutParams.topMargin = DisplayUtils.dip2px(this, 4);
+		navigationBar.addView(searchView, layoutParams);
+		
+		searchView.setOnQueryTextListener(this);
+		searchView.setQueryHint("请输入关键字");
+		
+		//初始化XListView
+		listView.setPullLoadEnable(true);
+		listView.setPullRefreshEnable(true);
+		listView.setXListViewListener(this);
+//		listAdapter = new TGListAdapter<T>(this, null, onvertViewLayoutId, viewHolderClass);
+//		listView.setAdapter(listAdapter);
+	}
+
+	@Override
+	public void onLoadMore()
+	{
+		//TODO 搜索请求,referId，为列表最后一行数据的标识，根据服务自行确定
+//		listAdapter.getLastItem();
+		requestSearch(searchView.getQueryText(), 0);
+	}
+
+	@Override
+	public void onRefresh()
+	{
+		
+	}
+	
+	private void requestSearch(String queryText, int referId)
+	{
+		
+	}
+
+	@Override
+	public void onQueryTextChange(CharSequence queryText)
+	{
+	}
+
+	@Override
+	public void onQueryTextSubmit(CharSequence queryText)
+	{
+		if(!TextUtils.isEmpty(queryText))
+		{
+			requestSearch(queryText.toString(), 0);
+			return;
+		}
+		
+		ToastUtils.showToast(this, "您输入的内容不能为空");
+	}
+
+	@Override
+	public void onTextCleaned()
+	{
+	}
+	
 }
