88101798791aab5e9bc12dafc7b12a084caf03b2
tiger <664674103@qq.com>
2014-12-12 11:41:55 +0800

    修改enMsg、cnMsg可见

--- Tiger/src/com/mn/tiger/request/error/TGErrorMsgEnum.java
+++ Tiger/src/com/mn/tiger/request/error/TGErrorMsgEnum.java
@@ -13,32 +13,41 @@ import com.mn.tiger.utility.CR;
 import com.mn.tiger.utility.Commons;
 import com.mn.tiger.utility.StringUtils;
 
+/**
+ * Http请求异常
+ */
 public enum TGErrorMsgEnum
 {
+	NO_NETWORK(10009, "无网络连接，请确认网络设置是否正确", "No network"),
+	IOEXCEPTION(10011, "请求异常，请稍后再试","Request Error,Please try again later"), 
+	UNKNOW_EXCEPTION(10012, "未知错误", "Unknow error"), 
+	SOCKET_TIMEOUT(10013, "连接超时,请稍后再试", "Connection timed out, please try again later"), 
+	JSON_FORMAT_ERROR(10008, "Json数据格式不正确", "The format of json is error."),
+	
 	/** 文件下载错误码 */
-	FAILED_CHECK_FILE_MD5(10001, "下载文件与服务端文件MD5校验不一致", "download file MD5 is not the same with server."), FAILED_GET_FILE_SIZE(
-			10002, "获取文件大小失败", "Failed to get file size"), DOWNLOAD_SAVE_PATH_IS_NULL(10003, "下载保存地址为空",
-			"The save path is null"), NOT_SUPPORT_BREAKPOINT(10004, "该服务不支持断点下载",
-			"This service not support breakpoint download."), SDCARD_NOT_AVALIABLE(10005, "内存卡不可用",
-			"The sdCard is not avaliable"), USABLE_SPACE_NOT_ENOUGH(10006, "可用空间不足，请清理空间",
-			"The usable space is not enough"), FAILED_DELETE_FILE(10007, "文件删除失败", "Failed to delete file"), JSON_FORMAT_ERROR(
-			10008, "Json数据格式不正确", "The format of json is error."), NO_NETWORK(10009, "无网络连接，请确认网络设置是否正确", "No network"),
+	FAILED_CHECK_FILE_MD5(10001, "下载文件与服务端文件MD5校验不一致", "download file MD5 is not the same with server."),
+	FAILED_GET_FILE_SIZE(10002, "获取文件大小失败", "Failed to get file size"), 
+	DOWNLOAD_SAVE_PATH_IS_NULL(10003, "下载保存地址为空","The save path is null"), 
+	NOT_SUPPORT_BREAKPOINT(10004, "该服务不支持断点下载","This service not support breakpoint download."), 
+	SDCARD_NOT_AVALIABLE(10005, "内存卡不可用","The sdCard is not avaliable"), 
+	USABLE_SPACE_NOT_ENOUGH(10006, "可用空间不足，请清理空间","The usable space is not enough"), 
+	FAILED_DELETE_FILE(10007, "文件删除失败", "Failed to delete file"), 
+	
+	FAILED_LOCAL_DATABASE(10010, "本地数据库异常", "local database error"), 
+	UNKNOWHOST_EXCEPTION(10014,"服务器连接异常", "Unknow host"),
 
-	FAILED_LOCAL_DATABASE(10010, "本地数据库异常", "local database error"), IOEXCEPTION(10011, "请求异常，请稍后再试",
-			"Request Error,Please try again later"), UNKNOW_EXCEPTION(10012, "未知错误", "Unknow error"), SOCKET_TIMEOUT(
-			10013, "连接超时,请稍后再试", "Connection timed out, please try again later"), UNKNOWHOST_EXCEPTION(10014,
-			"服务器连接异常", "Unknow host"),
-
-	SERVER_EXCEPTION(10015, "服务异常，请联系管理员", "Service is error,Please contact the admin"), VERIFY_UNSUCCESSFULLY(10016,
-			"校验失败", "Verify unsuccessfully."), SYSTEM_ERROR(10017, "系统错误", "System error"), FILE_NOT_EXITS(10018,
-			"文件不存在", "file not exits"), FILE_IS_EMPTY(10019, "文件内容为空", "file is empty"),
+	SERVER_EXCEPTION(10015, "服务异常，请联系管理员", "Service is error,Please contact the admin"), 
+	VERIFY_UNSUCCESSFULLY(10016,"校验失败", "Verify unsuccessfully."), 
+	SYSTEM_ERROR(10017, "系统错误", "System error"), 
+	FILE_NOT_EXITS(10018,"文件不存在", "file not exits"), 
+	FILE_IS_EMPTY(10019, "文件内容为空", "file is empty"),
 
 	// 定位异常
 	LOCATE_CONFIGURATION_ERROR(10020, "地图配置异常", "Configuration error of Map");
 
 	public final int code;
-	private String cnMsg = "";
-	private String enMsg = "";
+	public final String cnMsg;
+	public final String enMsg;
 
 	private TGErrorMsgEnum(int code, String cnMsg, String enMsg)
 	{
