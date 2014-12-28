2046b7f4d71b4984c3442a5fdd037c14feca5690
SmallTigerBrother <664674103@qq.com>
2014-12-28 21:21:44 +0800

    删除错误的代码

--- TigerDemo/src/com/mn/tiger/demo/widget/pulltorefresh/PullToRefreshSwipeListViewActivity.java
+++ TigerDemo/src/com/mn/tiger/demo/widget/pulltorefresh/PullToRefreshSwipeListViewActivity.java
@@ -11,9 +11,9 @@ import android.widget.TextView;
 import com.mn.tiger.annonation.ViewById;
 import com.mn.tiger.app.TGActionBarActivity;
 import com.mn.tiger.demo.R;
+import com.mn.tiger.demo.widget.swipelistview.SwipeListViewActivity.ViewHolder;
 import com.mn.tiger.utility.ViewInjector;
 import com.mn.tiger.widget.adpter.TGListAdapter;
-import com.mn.tiger.widget.adpter.TGSlidingViewHolder;
 
 public class PullToRefreshSwipeListViewActivity extends TGActionBarActivity
 {
@@ -38,29 +38,6 @@ public class PullToRefreshSwipeListViewActivity extends TGActionBarActivity
 		listAdapter = new TGListAdapter<Integer>(this, integers,
 				-1, ViewHolder.class);
 		swipeListView.setAdapter(listAdapter);
-		swipeListView.setOnItemLongClickListener(TGSlidingViewHolder.newSlidingOnItemLongClickListener());
-		swipeListView.setOnScrollListener(TGSlidingViewHolder.newSlidingOnScrollListener());
 	}
 	
-	public static class ViewHolder extends TGSlidingViewHolder<Integer>
-	{
-		public View initContentView()
-		{
-			return LayoutInflater.from(getActivity()).inflate(R.layout.swipe_list_item, null);
-		}
-		
-		public View initMenu()
-		{
-			TextView textView =  new TextView(getActivity());
-			textView.setText("不删除");
-			textView.setPadding(20, 10, 20, 10);
-			return textView;
-		}
-		
-		@Override
-		public void fillData(Integer itemData, int position)
-		{
-			
-		}
-	}
 }
