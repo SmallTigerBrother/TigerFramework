d09d335e12619c3ff78e14b186a7fa8593cccd81
SmallTigerBrother <664674103@qq.com>
2014-12-29 23:41:02 +0800

    修改计算backview高度的代码

--- TigerExtra/src/com/mn/tiger/widget/swipelistview/SwipeListViewHolder.java
+++ TigerExtra/src/com/mn/tiger/widget/swipelistview/SwipeListViewHolder.java
@@ -3,6 +3,7 @@ package com.mn.tiger.widget.swipelistview;
 import android.view.View;
 import android.view.View.MeasureSpec;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver.OnPreDrawListener;
 
 import com.mn.tiger.widget.adpter.TGViewHolder;
 
@@ -70,8 +71,19 @@ public abstract class SwipeListViewHolder<T> extends TGViewHolder<T>
 	@Override
 	protected void updateViewDimension(ViewGroup parent, View convertView, T itemData, int position)
 	{
+		frontView.getViewTreeObserver().addOnPreDrawListener(new OnPreDrawListener()
+		{
+			@Override
+			public boolean onPreDraw()
+			{
+				backView.getLayoutParams().height = frontView.getMeasuredHeight();
+				frontView.getViewTreeObserver().removeOnPreDrawListener(this);
+				return false;
+			}
+		});
 		//设置列表侧边栏宽度
 		backView.measure(MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST), 0);
+		
 		((SwipeListView)parent).setSwipeOffsetLeft(parent.getMeasuredWidth() - 
 				backView.getMeasuredWidth());
 	}
