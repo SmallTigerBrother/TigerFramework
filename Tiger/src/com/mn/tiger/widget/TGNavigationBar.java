8bd83ca925282adb31a45c3f0df5c5a5944aa5a0
tiger <664674103@qq.com>
2015-01-04 17:27:04 +0800

    修改导航条实现

--- Tiger/src/com/mn/tiger/widget/TGNavigationBar.java
+++ Tiger/src/com/mn/tiger/widget/TGNavigationBar.java
@@ -1,12 +1,16 @@
 package com.mn.tiger.widget;
 
+import org.json.JSONObject;
+
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.mn.tiger.R;
+import com.mn.tiger.utility.DisplayUtils;
 
 
 /**
@@ -24,19 +28,39 @@ public class TGNavigationBar extends RelativeLayout
 	protected final String LOG_TAG = this.getClass().getSimpleName();
 	
 	/**
+	 * 中间标题字体大小
+	 */
+	public static final float MIDDLE_TEXT_SIZE = 18f;
+	
+	/**
+	 * 中间标题字体颜色
+	 */
+	public static final int MIDDLE_TEXT_COLOR = 0xFF000000;
+	
+	/**
+	 * 按钮字体大小
+	 */
+	public static final float BUTTON_TEXT_SIZE = 14f;
+	
+	/**
+	 * 按钮字体颜色
+	 */
+	public static final int BUTTON_TEXT_COLOR = 0xFF000000;
+	
+	/**
 	 * 左导航区Layout
 	 */
-	private RelativeLayout leftNaviLayout;
+	private LinearLayout leftNaviLayout;
 	
 	/**
 	 * 右导航区Layout
 	 */
-	private RelativeLayout rightNaviLayout;
+	private LinearLayout rightNaviLayout;
 	
 	/**
 	 * 中间导航区Layout
 	 */
-	private RelativeLayout middleNaviLayout;
+	private LinearLayout middleNaviLayout;
 	
 	/**
 	 * 默认左导航按钮
@@ -72,16 +96,30 @@ public class TGNavigationBar extends RelativeLayout
 	 */
 	protected void setupViews()
 	{
-		LayoutInflater.from(getContext()).inflate(R.layout.tiger_navigationbar, this);
+		LayoutParams leftParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		leftParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+		leftParams.addRule(RelativeLayout.CENTER_VERTICAL);
+		this.addView(getLeftNaviLayout(), leftParams);
 		
-		leftNaviLayout = (RelativeLayout) findViewById(R.id.tiger_navi_left_layout);
-		leftNaviButton = (TGImageButton)findViewById(R.id.tiger_navi_left_btn);
+		getLeftNaviLayout().addView(getLeftNaviButton());
 		
-		rightNaviLayout = (RelativeLayout)findViewById(R.id.tiger_navi_right_layout);
-		rightNaviButton = (TGImageButton)findViewById(R.id.tiger_navi_right_btn);
+		LayoutParams rightParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		rightParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+		rightParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+		this.addView(getRightNaviLayout(), rightParams);
 		
-		middleNaviLayout = (RelativeLayout)findViewById(R.id.tiger_navi_middle_layout);
-		middleTextView = (TextView)findViewById(R.id.tiger_navi_middle_text);
+		getRightNaviLayout().addView(getRightNaviButton());
+		
+		LayoutParams middleParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		middleParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+		middleParams.addRule(RelativeLayout.CENTER_VERTICAL);
+		
+		this.addView(getMiddleNaviLayout(), middleParams);
+		
+		LinearLayout.LayoutParams middleTextParams = new LinearLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		middleTextParams.gravity = Gravity.CENTER_VERTICAL;
+		getMiddleNaviLayout().addView(getMiddleTextView(), middleTextParams);
 	}
 	
 	/**
@@ -90,8 +128,15 @@ public class TGNavigationBar extends RelativeLayout
 	 * @date 2013-8-30
 	 * @return
 	 */
-	public RelativeLayout getLeftNaviLayout()
+	public LinearLayout getLeftNaviLayout()
 	{
+		if(null == leftNaviLayout)
+		{
+			leftNaviLayout = new LinearLayout(getContext());
+			leftNaviLayout.setHorizontalGravity(Gravity.CENTER_VERTICAL);
+			leftNaviLayout.setPadding(DisplayUtils.dip2px(getContext(), 8), 0, 0, 0);
+		}
+		
 		return leftNaviLayout;
 	}
 	
@@ -101,8 +146,15 @@ public class TGNavigationBar extends RelativeLayout
 	 * @date 2013-8-30
 	 * @return
 	 */
-	public RelativeLayout getRightNaviLayout()
+	public LinearLayout getRightNaviLayout()
 	{
+		if(null == rightNaviLayout)
+		{
+			rightNaviLayout = new LinearLayout(getContext());
+			rightNaviLayout.setHorizontalGravity(Gravity.CENTER_VERTICAL);
+			rightNaviLayout.setPadding(0, 0, DisplayUtils.dip2px(getContext(), 8), 0);
+		}
+		
 		return rightNaviLayout;
 	} 
 	
@@ -112,8 +164,17 @@ public class TGNavigationBar extends RelativeLayout
 	 * @date 2013-8-30
 	 * @return
 	 */
-	public RelativeLayout getMiddleNaviLayout()
+	public LinearLayout getMiddleNaviLayout()
 	{
+		if(null == middleNaviLayout)
+		{
+			middleNaviLayout = new LinearLayout(getContext());
+			middleNaviLayout.setOrientation(LinearLayout.HORIZONTAL);
+			middleNaviLayout.setHorizontalGravity(Gravity.CENTER_VERTICAL);
+			middleNaviLayout.setPadding(DisplayUtils.dip2px(getContext(), 50), 0, 
+					DisplayUtils.dip2px(getContext(), 50), 0);
+		}
+		
 		return middleNaviLayout;
 	}
 	
@@ -125,7 +186,36 @@ public class TGNavigationBar extends RelativeLayout
 	 */
 	public TGImageButton getLeftNaviButton()
 	{
+		if(null == leftNaviButton)
+		{
+			leftNaviButton = new TGImageButton(getContext());
+			leftNaviButton.setVisibility(View.INVISIBLE);
+			leftNaviButton.setTextColor(BUTTON_TEXT_COLOR);
+			leftNaviButton.setTextSize(BUTTON_TEXT_SIZE);
+			leftNaviButton.setPadding(DisplayUtils.dip2px(getContext(), 8),
+					DisplayUtils.dip2px(getContext(), 8), 
+					DisplayUtils.dip2px(getContext(), 8), 
+					DisplayUtils.dip2px(getContext(), 8));
+			
+			LinearLayout.LayoutParams leftBtnParams = new LinearLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
+			leftNaviButton.setLayoutParams(leftBtnParams);
+		}
+		
 		return leftNaviButton;
+	}
+	
+	/**
+	 * 该方法的作用:
+	 * 设置左侧导航按钮
+	 * @date 2014年3月23日
+	 * @param leftNaviButton
+	 */
+	public void setLeftNaviButton(TGImageButton leftNaviButton)
+	{
+		leftNaviLayout.removeAllViews();
+		this.leftNaviButton = leftNaviButton;
+		leftNaviLayout.addView(leftNaviButton);
 	}
 	
 	/**
@@ -136,7 +226,36 @@ public class TGNavigationBar extends RelativeLayout
 	 */
 	public TGImageButton getRightNaviButton()
 	{
+		if(null == rightNaviButton)
+		{
+			rightNaviButton = new TGImageButton(getContext());
+			rightNaviButton.setVisibility(View.INVISIBLE);
+			rightNaviButton.setTextColor(BUTTON_TEXT_COLOR);
+			rightNaviButton.setTextSize(BUTTON_TEXT_SIZE);
+			rightNaviButton.setPadding(DisplayUtils.dip2px(getContext(), 8),
+					DisplayUtils.dip2px(getContext(), 8), 
+					DisplayUtils.dip2px(getContext(), 8), 
+					DisplayUtils.dip2px(getContext(), 8));
+			
+			LinearLayout.LayoutParams rightBtnParams = new LinearLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
+			rightNaviButton.setLayoutParams(rightBtnParams);
+		}
+		
 		return rightNaviButton;		
+	}
+	
+	/**
+	 * 该方法的作用:
+	 * 设置右侧导航按钮
+	 * @date 2014年3月23日
+	 * @param rightNaviButton
+	 */
+	public void setRightNaviButton(TGImageButton rightNaviButton)
+	{
+		rightNaviLayout.removeAllViews();
+		this.rightNaviButton = rightNaviButton;
+		rightNaviLayout.addView(rightNaviButton);
 	}
 	
 	/**
@@ -147,6 +266,15 @@ public class TGNavigationBar extends RelativeLayout
 	 */
 	public TextView getMiddleTextView()
 	{
+		if(null == middleTextView)
+		{
+			middleTextView = new TGMarqueeText(getContext());
+			middleTextView.setSingleLine();
+			middleTextView.setTextColor(MIDDLE_TEXT_COLOR);
+			middleTextView.setTextSize(MIDDLE_TEXT_SIZE);
+			middleTextView.setGravity(Gravity.CENTER);
+		}
+		
 		return middleTextView;
 	}
 	
@@ -172,6 +300,26 @@ public class TGNavigationBar extends RelativeLayout
 		
 	/**
 	 * 该方法的作用:
+	 * 设置中间标题文本
+	 * @date 2013-8-30
+	 * @param text
+	 * @param options 文本显示参数
+	 * @return
+	 */
+	public boolean setMiddleText(String text, JSONObject options)
+	{
+		TextView middleText = getMiddleTextView();
+		
+		if (null != middleText) 
+		{
+			middleText.setText(text);
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * 该方法的作用:
 	 * 设置左导航按钮是否可用
 	 * @date 2013-8-30
 	 * @param enabled
@@ -190,5 +338,26 @@ public class TGNavigationBar extends RelativeLayout
 	public void setRightButtonEnabled(boolean enabled) 
 	{
 		getRightNaviButton().setEnabled(enabled);
+	}
+	
+	/**
+	 * 布局参数LayoutParams
+	 */
+	public static class LayoutParams extends RelativeLayout.LayoutParams
+	{
+		public LayoutParams(int w, int h)
+		{
+			super(w, h);
+		}
+		
+		public LayoutParams(MarginLayoutParams source)
+		{
+			super(source);
+		}
+		
+		public LayoutParams(RelativeLayout.LayoutParams source)
+		{
+			super(source);
+		}
 	}
 }
