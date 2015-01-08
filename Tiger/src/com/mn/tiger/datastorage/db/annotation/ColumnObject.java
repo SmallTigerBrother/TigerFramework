51b043ce7c25809242829edb0a687ced1ccd2d17
tiger <664674103@qq.com>
2015-01-08 16:42:56 +0800

    新增注释

--- Tiger/src/com/mn/tiger/datastorage/db/annotation/ColumnObject.java
+++ Tiger/src/com/mn/tiger/datastorage/db/annotation/ColumnObject.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 注解类，用于添加列名,仅注释对象类型的参数
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ColumnObject
