a2b6a49ede86b0c869e4393ff0d3b8ead9a88042
SmallTigerBrother <664674103@qq.com>
2015-01-22 21:41:14 +0800

    修改TGTabActivity

--- Tiger/src/com/mn/tiger/app/TGTabActivity.java
+++ Tiger/src/com/mn/tiger/app/TGTabActivity.java
@@ -1,22 +1,40 @@
 package com.mn.tiger.app;
 
-import java.util.List;
+import java.util.Arrays;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.mn.tiger.R;
+import com.mn.tiger.widget.adpter.TGListAdapter;
+import com.mn.tiger.widget.adpter.TGViewHolder;
 import com.mn.tiger.widget.tab.TGTabView;
+import com.mn.tiger.widget.tab.TGTabView.LayoutParams;
 import com.mn.tiger.widget.tab.TGTabView.OnTabChangeListener;
 import com.mn.tiger.widget.viewpager.TGFragmentPagerAdapter;
 
-public abstract class TGTabActivity<T> extends TGActionBarActivity implements 
+/**
+ * 带底部Tab的FragmentActivity
+ * @author Dalang
+ */
+public abstract class TGTabActivity extends TGActionBarActivity implements 
     OnPageChangeListener, OnTabChangeListener
 {
+	/**
+	 * 底部Tab
+	 */
 	private TGTabView tabView;
 	
+	/**
+	 * ViewPager
+	 */
 	private ViewPager viewPager;
 	
 	@Override
@@ -29,25 +47,84 @@ public abstract class TGTabActivity<T> extends TGActionBarActivity implements
 		tabView = (TGTabView) findViewById(R.id.tiger_tab_bar);
 		viewPager = (ViewPager) findViewById(R.id.tiger_view_pager);
 		
-		
-		TGFragmentPagerAdapter pagerAdapter = new TGFragmentPagerAdapter(
-				getSupportFragmentManager(), initFragments());
-		viewPager.setAdapter(pagerAdapter);
-		
 		tabView.setOnTabChangeListener(this);
-		viewPager.setOnPageChangeListener(this);
 	}
 	
-	protected abstract List<Fragment> initFragments();
+	/**
+	 * 设置Tab和Fragment
+	 * @param tabModels 所有的tab
+	 * @param fragments 所有的fragment
+	 */
+	public void setTabs(TabModel[] tabModels, Fragment[] fragments)
+	{
+		TGFragmentPagerAdapter pagerAdapter = new TGFragmentPagerAdapter(
+				getSupportFragmentManager(), Arrays.asList(fragments));
+		viewPager.setAdapter(pagerAdapter);
+		viewPager.setOnPageChangeListener(this);
+		
+		tabView.setAdapter(new TGListAdapter<TabModel>(this, Arrays.asList(tabModels),
+				R.layout.tiger_fragment_tab_item, TabViewHolder.class));
+		tabView.setSelection(0);
+	}
 
-	protected abstract void initTabView(TGTabView taView);
-	
-	public TGTabView getTabView()
+	@Override
+	public void onTabChanged(TGTabView tabView, int lastTabIndex, int currentTabIndex)
+	{
+		//重置上一个选中的tab，如果从来没有设置过选中项，lastTabIndex== -1
+		if(lastTabIndex >= 0)
+		{
+			resetLastTab(tabView, lastTabIndex);
+		}
+		
+		//高亮显示当前Tab
+		highLightCurrentTab(tabView, currentTabIndex);
+		
+		//设置ViewPager显示的页面
+		viewPager.setCurrentItem(currentTabIndex, false);
+	}
+
+	/**
+	 * 重置上一个tab的资源
+	 * @param tabView
+	 * @param lastTabIndex 上一个tab的索引
+	 */
+	protected void resetLastTab(TGTabView tabView, int lastTabIndex)
+	{
+		TabViewHolder holder = (TabViewHolder) tabView.getTabItem(lastTabIndex).getConvertView().getTag();
+		TabModel tabModel = (TabModel) tabView.getAdapter().getItem(lastTabIndex);
+		holder.getImageView().setImageResource(tabModel.getDefaultResId());
+		holder.getTextView().setTextColor(tabModel.getDefaultTextColor());
+		holder.getTextView().setTextSize(tabModel.getDefaultTextSize());
+	}
+
+	/**
+	 * 高亮当前选中的的tab
+	 * @param tabView
+	 * @param currentTabIndex 当前tab的索引
+	 */
+	protected void highLightCurrentTab(TGTabView tabView, int currentTabIndex)
+	{
+		TabViewHolder holder = (TabViewHolder) tabView.getTabItem(currentTabIndex).getConvertView().getTag();
+		TabModel tabModel = (TabModel) tabView.getAdapter().getItem(currentTabIndex);
+		holder.getImageView().setImageResource(tabModel.getHighlightResId());
+		holder.getTextView().setTextColor(tabModel.getHighlightTextColor());
+		holder.getTextView().setTextSize(tabModel.getHighlightTextSize());
+	}
+
+	/**
+	 * 获取tabView
+	 * @return
+	 */
+	protected TGTabView getTabView()
 	{
 		return tabView;
 	}
 	
-	public ViewPager getViewPager()
+	/**
+	 * 获取ViewPager
+	 * @return
+	 */
+	protected ViewPager getViewPager()
 	{
 		return viewPager;
 	}
@@ -68,24 +145,163 @@ public abstract class TGTabActivity<T> extends TGActionBarActivity implements
 	public void onPageScrolled(int page, float arg1, int arg2)
 	{
 	}
-
-	@Override
-	public void onTabChanged(TGTabView tabView, int lastTabIndex, int currentTabIndex)
+	
+	/**
+	 * 自定义的TabViewHolder
+	 * @author Dalang
+	 */
+	public static class TabViewHolder extends TGViewHolder<TabModel>
 	{
-		//重置上一个选中的tab，如果从来没有设置过选中项，lastTabIndex== -1
-		if(lastTabIndex >= 0)
+		private ImageView imageView;
+		
+		private TextView textView;
+		
+		@Override
+		public View initView(View convertView, ViewGroup parent)
 		{
-			resetLastTab(tabView, lastTabIndex);
+			View view = super.initView(convertView, parent);
+			//设置tab均分
+			LayoutParams layoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, 
+					LayoutParams.WRAP_CONTENT, 1);
+			view.setLayoutParams(layoutParams);
+			
+			imageView = (ImageView) view.findViewById(R.id.tab_item_image);
+			textView = (TextView) view.findViewById(R.id.tab_item_name);
+			
+			return view;
 		}
 		
-		//高亮显示当前Tab
-		highLightCurrentTab(tabView, currentTabIndex);
+		@Override
+		public void fillData(ViewGroup parent, View convertView, TabModel itemData, int position)
+		{
+			imageView.setImageResource(itemData.getDefaultResId());
+			textView.setText(itemData.getTabName());
+		}
 		
-		//设置ViewPager显示的页面
-		viewPager.setCurrentItem(currentTabIndex, false);
+		public ImageView getImageView()
+		{
+			return imageView;
+		}
+		
+		public TextView getTextView()
+		{
+			return textView;
+		}
 	}
 	
-	protected abstract void resetLastTab(TGTabView tabView, int lastTabIndex);
-	
-	protected abstract void highLightCurrentTab(TGTabView tabView, int currentTabIndex);
+	/**
+	 * 底部Tab数据模型
+	 * @author Dalang
+	 */
+	protected static class TabModel
+	{
+		/**
+		 * 默认图片资源
+		 */
+		private int defaultResId;
+		
+		/**
+		 * 高亮图片资源
+		 */
+		private int highlightResId;
+		
+		/**
+		 * tab名称
+		 */
+		private String tabName;
+		
+		/**
+		 * 默认的文本颜色
+		 */
+		private int defaultTextColor = Color.BLACK;
+		
+		/**
+		 * 选中时高亮显示的文本颜色
+		 */
+		private int highlightTextColor = Color.BLACK;
+		
+		/**
+		 * 默认的文字大小
+		 */
+		private float defaultTextSize = 16f;
+		
+		/**
+		 * 选中时的文字大小
+		 */
+		private float highlightTextSize = 16f;
+		
+		public TabModel()
+		{
+		}
+
+		public int getDefaultResId()
+		{
+			return defaultResId;
+		}
+
+		public void setDefaultResId(int defaultResId)
+		{
+			this.defaultResId = defaultResId;
+		}
+
+		public String getTabName()
+		{
+			return tabName;
+		}
+
+		public void setTabName(String tabName)
+		{
+			this.tabName = tabName;
+		}
+
+		public int getHighlightResId()
+		{
+			return highlightResId;
+		}
+
+		public void setHighlightResId(int highlightResId)
+		{
+			this.highlightResId = highlightResId;
+		}
+
+		public int getDefaultTextColor()
+		{
+			return defaultTextColor;
+		}
+
+		public void setDefaultTextColor(int defaultTextColor)
+		{
+			this.defaultTextColor = defaultTextColor;
+		}
+
+		public int getHighlightTextColor()
+		{
+			return highlightTextColor;
+		}
+
+		public void setHighlightTextColor(int highlightTextColor)
+		{
+			this.highlightTextColor = highlightTextColor;
+		}
+
+		public float getDefaultTextSize()
+		{
+			return defaultTextSize;
+		}
+
+		public void setDefaultTextSize(float defaultTextSize)
+		{
+			this.defaultTextSize = defaultTextSize;
+		}
+
+		public float getHighlightTextSize()
+		{
+			return highlightTextSize;
+		}
+
+		public void setHighlightTextSize(float highlightTextSize)
+		{
+			this.highlightTextSize = highlightTextSize;
+		}
+	}
 }
--- TigerDemo/src/com/mn/tiger/demo/widget/viewpager/horizontal/fragment/ViewPagerWithFragmentActivity.java
+++ TigerDemo/src/com/mn/tiger/demo/widget/viewpager/horizontal/fragment/ViewPagerWithFragmentActivity.java
@@ -1,45 +1,21 @@
 package com.mn.tiger.demo.widget.viewpager.horizontal.fragment;
 
-import java.util.ArrayList;
-
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.mn.tiger.annonation.ViewById;
-import com.mn.tiger.app.TGActionBarActivity;
+import com.mn.tiger.app.TGTabActivity;
 import com.mn.tiger.demo.R;
-import com.mn.tiger.utility.ViewInjector;
-import com.mn.tiger.widget.adpter.TGListAdapter;
-import com.mn.tiger.widget.adpter.TGViewHolder;
-import com.mn.tiger.widget.tab.TGTabView;
-import com.mn.tiger.widget.tab.TGTabView.LayoutParams;
 import com.mn.tiger.widget.tab.TGTabView.OnTabChangeListener;
-import com.mn.tiger.widget.viewpager.TGFragmentPagerAdapter;
 
 /**
  * 实现类似底部带Tab，分页是Fragment的界面
  * @author Dalang
  */
-public class ViewPagerWithFragmentActivity extends TGActionBarActivity implements 
+public class ViewPagerWithFragmentActivity extends TGTabActivity implements 
     OnTabChangeListener, OnPageChangeListener
 {
-	@ViewById(id = R.id.view_pager)
-	private ViewPager viewPager;
-	
-	@ViewById(id = R.id.view_pager_tab)
-	private TGTabView tabView;
-	
-	/**
-	 * 填充到ViewPager中的Fragment数组
-	 */
-	private ArrayList<Fragment> fragments;
-	
 	/**
 	 * tab默认显示的资源
 	 */
@@ -61,145 +37,25 @@ public class ViewPagerWithFragmentActivity extends TGActionBarActivity implement
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.viewpager_fragment_tabview_activity);
-		ViewInjector.initInjectedView(this, this);
 		
-		setupViews();
-	}
-
-	private void setupViews()
-	{
-		//初始化ViewPager
-		fragments = new ArrayList<Fragment>();
-		fragments.add(new PagerFragment_1());
-		fragments.add(new PagerFragment_2());
-		fragments.add(new PagerFragment_3());
+		Fragment[] fragments = new Fragment[3];
+		fragments[0] = new PagerFragment_1();
+		fragments[1] = new PagerFragment_2();
+		fragments[2] = new PagerFragment_3();
 		
-		viewPager.setAdapter(new TGFragmentPagerAdapter(getSupportFragmentManager(), 
-				fragments));
-		viewPager.setOnPageChangeListener(this);
-		
-		//初始化TabView
-		ArrayList<TabModel> tabModels = new ArrayList<TabModel>();
+		TabModel[] tabModels = new TabModel[3];
 		TabModel tabModel;
 		for(int i = 0 ; i < tabNames.length; i++)
 		{
 			tabModel = new TabModel();
-			tabModel.setImageResId(tabDefaultIconResId[i]);
+			tabModel.setDefaultResId(tabDefaultIconResId[i]);
 			tabModel.setTabName(tabNames[i]);
 			tabModel.setHighlightResId(tabHighLightIconResId[i]);
-			tabModels.add(tabModel);
+			tabModel.setHighlightTextColor(Color.BLUE);
+			tabModel.setHighlightTextSize(24f);
+			tabModels[i] = tabModel;
 		}
 		
-		tabView.setAdapter(new TGListAdapter<TabModel>(this, tabModels,
-				R.layout.fragment_tab_item, TabViewHolder.class));
-		tabView.setOnTabChangeListener(this);
-		tabView.setSelection(0);
-	}
-	
-	@Override
-	public void onTabChanged(TGTabView tabView, int lastTabIndex, int currentTabIndex)
-	{
-		// 还原上一个选中的tab, 若lastTabIndex > 0，则说明存在已选中的tab
-		if (lastTabIndex >= 0)
-		{
-			TabViewHolder holder = (TabViewHolder) tabView.getTabItem(lastTabIndex).getConvertView().getTag();
-			TabModel tabModel = (TabModel) tabView.getAdapter().getItem(lastTabIndex);
-			holder.getImageView().setImageResource(tabModel.getImageResId());
-		}
-		
-		// 高亮显示当前选中的tab
-		TabViewHolder holder = (TabViewHolder) tabView.getTabItem(currentTabIndex).getConvertView().getTag();
-		TabModel tabModel = (TabModel) tabView.getAdapter().getItem(currentTabIndex);
-		holder.getImageView().setImageResource(tabModel.getHighlightResId());
-
-		// 切换Page
-		viewPager.setCurrentItem(currentTabIndex, false);
-	}
-
-	@Override
-	public void onPageSelected(int page)
-	{
-		tabView.setSelection(page);
-	}
-	
-	@Override
-	public void onPageScrollStateChanged(int state)
-	{
-	}
-
-	@Override
-	public void onPageScrolled(int page, float arg1, int arg2)
-	{
-	}
-	
-	public class TabViewHolder extends TGViewHolder<TabModel>
-	{
-		@ViewById(id = R.id.tab_item_image)
-		private ImageView imageView;
-		
-		@ViewById(id = R.id.tab_item_name)
-		private TextView textView;
-		
-		@Override
-		public View initView(View convertView, ViewGroup parent)
-		{
-			View view = super.initView(convertView, parent);
-			LayoutParams layoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, 
-					LayoutParams.WRAP_CONTENT, 1);
-			view.setLayoutParams(layoutParams);
-			return view;
-		}
-		
-		@Override
-		public void fillData(ViewGroup parent, View convertView, TabModel itemData, int position)
-		{
-			imageView.setImageResource(itemData.getImageResId());
-			textView.setText(itemData.getTabName());
-		}
-		
-		public ImageView getImageView()
-		{
-			return imageView;
-		}
-	}
-	
-	public static class TabModel
-	{
-		private int imageResId;
-		
-		private int highlightResId;
-		
-		private String tabName;
-
-		public int getImageResId()
-		{
-			return imageResId;
-		}
-
-		public void setImageResId(int imageResId)
-		{
-			this.imageResId = imageResId;
-		}
-
-		public String getTabName()
-		{
-			return tabName;
-		}
-
-		public void setTabName(String tabName)
-		{
-			this.tabName = tabName;
-		}
-
-		public int getHighlightResId()
-		{
-			return highlightResId;
-		}
-
-		public void setHighlightResId(int highlightResId)
-		{
-			this.highlightResId = highlightResId;
-		}
+		setTabs(tabModels, fragments);
 	}
 }
