9426493568b9827dbfbfb8e518c458af7fb9cc4c
tiger <664674103@qq.com>
2015-01-31 10:59:42 +0800

    新增getDirSize方法

--- Tiger/src/com/mn/tiger/utility/FileUtils.java
+++ Tiger/src/com/mn/tiger/utility/FileUtils.java
@@ -829,4 +829,38 @@ public class FileUtils
 		
 		return null;
 	}
+	
+	/**
+	 * 获取目录/文件大小
+	 * @param file
+	 * @return
+	 */
+	public static double getDirSize(File file)
+	{
+		// 判断文件是否存在
+		if (file.exists())
+		{
+			// 如果是目录则递归计算其内容的总大小
+			if (file.isDirectory())
+			{
+				File[] children = file.listFiles();
+				double size = 0;
+				for (File f : children)
+				{
+					size += getDirSize(f);
+				}
+				return size;
+			}
+			else
+			{// 如果是文件则直接返回其大小,以“兆”为单位
+				double size = (double) file.length() / 1024 / 1024;
+				return size;
+			}
+		}
+		else
+		{
+			LogTools.d(LOG_TAG, "file is not exist, please check the path of file");
+			return 0.0;
+		}
+	}
 }
