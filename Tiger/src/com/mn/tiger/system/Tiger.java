eb6da0471c85176bbd985c84e4754d34d9be053e
smallTigerBrother <664674103@qq.com>
2015-05-25 16:20:14 +0800

    提升版本号

--- Tiger/src/com/mn/tiger/system/Tiger.java
+++ Tiger/src/com/mn/tiger/system/Tiger.java
@@ -11,7 +11,7 @@ public class Tiger
 	/**
 	 * 当前使用版本
 	 */
-	public static final VERSION_CODES SDK_CODE = VERSION_CODES.SOUTH_CHINA_TIGER;
+	public static final VERSION_CODES SDK_CODE = VERSION_CODES.BENGAL_TIGER;
 	
 	/**
 	 * 该类作用及功能说明:版本代号
@@ -20,7 +20,9 @@ public class Tiger
 	 */
 	public static enum VERSION_CODES
 	{
-		SOUTH_CHINA_TIGER("1.0.0", 1);
+		SOUTH_CHINA_TIGER("1.0.0", 1),
+		//升级最低适配版本为API 15
+		BENGAL_TIGER("2.0.0", 2);
 		
 		// release版本号
 		public String RELEASE;
